f6ed78737518db4a43b04455809f34f16478ae6c
pangkoki20 <sasithorn.pangkoki@gmail.com>
2021-04-26 00:07:25 +0700

    Form Home

--- covid19/pages/index.js
+++ covid19/pages/index.js
@@ -1,21 +1,40 @@
-import Head from 'next/image'
+import Link from 'next/link'
+import Image from 'next/image'
+
 import styles from '../styles/Home.module.css'
-import { Col, Row, Button, Form, FormGroup, Label, Input, Container, image } from 'reactstrap';
+import { Col, Row, Button, Form, FormGroup, Label, Input, Container, Nav, NavItem, NavLink } from 'reactstrap';
 
 
 export default function Home() {
   return (
     <div>
       <Container>
-        <Form>
-          <image
-            src="Vaccine.png"
+        <div>
+          <li>
+            <Link href="/">
+              <a>Home</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/about">
+              <a>About Us</a>
+            </Link>
+          </li>
+          <li>
+            <Link href="/blog/hello-world">
+              <a>Blog Post</a>
+            </Link>
+          </li>
+        </div>
+        <div>
+          <Image
+            src="/Image/Vaccine.png"
             alt="Picture of the author"
             width={500}
             height={500}
           />
-        </Form>
+        </div>
       </Container>
-    </div>
+    </div >
   )
 }
